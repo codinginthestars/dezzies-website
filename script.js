03ca129a2b474e1942167c7d68d0b220922dab2b
William <codinginthestars@gmail.com>
2024-11-16 15:55:29 -0600

    styles and functionality

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const themeToggle = document.getElementById('theme-toggle');
 
 themeToggle.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
-    const mode = document.body.classList.contains('dark-mode') ? 'Dark Mode' : 'Light Mode';
-    themeToggle.textContent = `Switch to ${mode}`;
+    const mode = document.body.classList.contains('dark-mode') ? 'Light' : 'Dark';
+    themeToggle.textContent = mode;
 });
 
 // Modal Gallery
@@ -17,6 +17,37 @@ galleryImages.forEach((img) => {
     img.addEventListener('click', () => {
         modal.style.display = 'block';
         modalImage.src = img.src;
+        modal.classList.add('show');
+        document.body.classList.add('modal-open');
     });
 });
 
+closeModal.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+closeModal.addEventListener('click', closeWithAnimation);
+
+modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+        modal.style.display = 'none';
+        closeWithAnimation();
+    }
+});
+
+function closeWithAnimation() {
+    modal.classList.remove('show');
+    modal.classList.add('hide');
+    document.body.classList.remove('modal-open');
+
+
+    setTimeout(() => {
+        modal.classList.remove('hide');
+        modal.style.display = 'none';
+    }, 300);
+};
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape' && modal.classList.contains('show')) {
+        closeWithAnimation();
+    }
+})
